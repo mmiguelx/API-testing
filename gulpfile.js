ff89d42933bfa8c5cb1f8bf0ee0d2b4b90a8e0ea
Mario Pernia <57371690+mmiguelx@users.noreply.github.com>
2021-03-22 23:19:31 +0100

    apistack call done

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,29 @@
-var gulp = require('gulp');
+var gulp 		= require('gulp');
 var browserSync = require('browser-sync').create();
-var reload = browserSync.reload;
+var sass		= require('gulp-sass');
+var reload 		= browserSync.reload;
+
+gulp.task('sass', function() {
+	return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+		.pipe(sass())
+		.pipe(gulp.dest("src/css"))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('js', function() {
+	return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js'])
+		.pipe(gulp.dest("src/js"))
+		.pipe(browserSync.stream());
+});
 
 gulp.task('server', function() {
 	browserSync.init({
 		server: {
-			baseDir: "./"
-		}
+			baseDir: "./src",
+		},
 	});
-
-	gulp.watch("*.html").on("change", reload);
+	gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], gulp.series('sass'));
+	gulp.watch("**/*.html").on("change", reload);
 });
+
+gulp.task('default', gulp.series('js', 'sass'));
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
-console.log("a");
+const access_key = '714d2f24e2803f9f751345fa0dab08d6'
+
+$.ajax({
+    url: 'http://api.ipstack.com/check?access_key=' + access_key,
+    dataType: 'jsonp',
+    success: function(json) {
+		console.log(json);
+		jsonStr = JSON.stringify(json);
+		console.log(jsonStr)
+	}
+});
