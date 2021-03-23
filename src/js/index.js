d7b55298aed4165dc9ff6dd2c60519782cb48437
Mario Pernia <57371690+mmiguelx@users.noreply.github.com>
2021-03-23 14:51:30 +0100

    json setter done

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,34 @@
 const access_key = '714d2f24e2803f9f751345fa0dab08d6'
 
+function jsonSetter(json) {
+	jsonStr = JSON.stringify(json);
+	jsonStr = jsonStr.replace(/{/g,'');
+	jsonStr = jsonStr.replace(/,/g,"<br>");
+	jsonStr = jsonStr.replace(/:/g,": ");
+	jsonStr = jsonStr.replace(/"/g,"");
+	jsonStr = jsonStr.replace(/}/g,"");
+	jsonStr = jsonStr.replace(/\[/g,"<ul><li>");
+	jsonStr = jsonStr.replace(/<br>code/g,"<li>code");
+	jsonStr = jsonStr.replace(/]/g,"</ul>");
+	jsonStr = jsonStr.replace("</ul><br>","</ul>");
+
+	return (jsonStr);
+}
+
 $.ajax({
     url: 'http://api.ipstack.com/check?access_key=' + access_key,
     dataType: 'jsonp',
     success: function(json) {
-		console.log(json);
-		jsonStr = JSON.stringify(json);
-		console.log(jsonStr)
+		/*jsonStr = JSON.stringify(json);
+		jsonStr = jsonStr.replace(/{/g,'');
+		jsonStr = jsonStr.replace(/,/g,"<br>");
+		jsonStr = jsonStr.replace(/:/g,": ");
+		jsonStr = jsonStr.replace(/"/g,"");
+		jsonStr = jsonStr.replace(/}/g,"");
+		jsonStr = jsonStr.replace(/\[/g,"<ul><li>");
+		jsonStr = jsonStr.replace(/<br>code/g,"<li>code");
+		jsonStr = jsonStr.replace(/]/g,"</ul>");
+		jsonStr = jsonStr.replace("</ul><br>","</ul>");*/
+		document.getElementById('test').innerHTML = jsonSetter(json);
 	}
 });
